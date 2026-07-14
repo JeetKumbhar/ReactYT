f421f9103dd6a01d76cf559c45481c6ec90e143a
JeetKumbhar <jeetkumbhar2210@gmail.com>
2026-07-14 22:42:06 +0530

    02-components created

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,7 @@
+export default function Card () {
+    return <div className="card">
+        <h1>Jeet Kumbhar</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, magnam?</p>
+    </div>
+
+}
