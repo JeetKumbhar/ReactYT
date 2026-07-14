37c8613356e303f8b1741d362a5cae1db1750dd6
JeetKumbhar <jeetkumbhar2210@gmail.com>
2026-07-14 23:56:00 +0530

    02-components changed

--- 02-components/src/components/Card.jsx
+++ 02-components/src/components/Card.jsx
@@ -1,7 +1,14 @@
-export default function Card () {
-    return <div className="card">
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+      <div className="card">
         <h1>Jeet Kumbhar</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, magnam?</p>
+      </div>
     </div>
-
+  )
 }
+
+export default card
